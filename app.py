3eba3a99e84ffddf18028d3849b535c0cf9f5d2b
M.G.Jillani <76724558+MGJillaniMughal@users.noreply.github.com>
2021-10-03 20:28:36 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
 import streamlit as st
 import pandas as pd
 
+# Link with Bootstrap for navigation
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+
+st.markdown("""
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3498DB;">
+  <a class="navbar-brand" href="https://github.com/MGJillaniMughal" target="_blank">M.G.Jillani Soft-Tech</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://github.com/MGJillaniMughal" target="_blank">Github</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://www.linkedin.com/in/muhammad-ghulam-jillani-38603820a/" target="_blank">LinkedIn</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+""", unsafe_allow_html=True)
+
 st.markdown('''# **Binance Price APP**
 A simple cryptocurrency price app pulling price data from *Binance API*.
 ''')
